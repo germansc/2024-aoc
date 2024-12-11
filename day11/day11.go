757a74bff63a5ced0a2777c19f78c6fc7eee2161
germansc <germanscillone@gmail.com>
2024-12-11 15:08:14 -0300

    [day11] Refactored and added a solution for part 2 of day 11.

--- day11/day11.go
+++ day11/day11.go
@@ -46,38 +46,81 @@ func solve(lines []string) {
 		stones = append(stones, v)
 	}
 
-	// Part 1. Process 25 blinks.
+	// Part 1.
 	part1 := blink(stones, 25)
-	fmt.Printf("Part 1: %v\n", len(part1))
+	fmt.Printf("Part 1: %v\n", part1)
 
-	part2 := 0
+	// Part 2.
+	part2 := blink(stones, 75)
 	fmt.Printf("Part 2: %v\n", part2)
 }
 
-func blink(stones []string, count int) []string {
-	fmt.Printf("%v\n", stones)
-	for range count {
-		buff := []string{}
-		for _, v := range stones {
-			// Apply the rules.
-			vi, _ := strconv.Atoi(v)
-			if vi == 0 {
-				buff = append(buff, "1")
-			} else if len(v)%2 == 0 {
-				// Convert intermediate strings to ints to remove leading zeros.
-				v1, _ := strconv.Atoi(v[:len(v)/2])
-				v2, _ := strconv.Atoi(v[len(v)/2:])
+// Cache of the next set of stones for each one.
+var cache = make(map[string][]string)
 
-				buff = append(buff, strconv.Itoa(v1))
-				buff = append(buff, strconv.Itoa(v2))
-			} else {
-				buff = append(buff, strconv.Itoa(vi*2024))
+func newstones(stone string) []string {
+	if r, ok := cache[stone]; ok {
+		return r
+	}
+
+	buff := []string{}
+	vi, _ := strconv.Atoi(stone)
+	if vi == 0 {
+		buff = append(buff, "1")
+	} else if len(stone)%2 == 0 {
+		// Convert intermediate strings to ints to remove leading zeros.
+		v1, _ := strconv.Atoi(stone[:len(stone)/2])
+		v2, _ := strconv.Atoi(stone[len(stone)/2:])
+
+		buff = append(buff, strconv.Itoa(v1))
+		buff = append(buff, strconv.Itoa(v2))
+	} else {
+		buff = append(buff, strconv.Itoa(vi*2024))
+	}
+
+	cache[stone] = buff
+	return buff
+}
+
+func blink(stones []string, count int) int {
+	// Process each uniq stone, keeping track of its count.
+	stoneQueue := make(map[string]int)
+	for _, stone := range stones {
+		stoneQueue[stone]++
+	}
+
+	fmt.Printf("\nStarting set: %v\n", stoneQueue)
+	for i := range count {
+		// Prepare a queue for the next iteration.
+		next := make(map[string]int)
+
+		// Process each uniq stone in the current queue.
+		for k, v := range stoneQueue {
+			ns := newstones(k)
+			// Append the resulting stones, for each copy of the current k.
+			for _, s := range ns {
+				next[s] += v
 			}
 		}
 
-		// fmt.Printf("> %v\n", buff)
-		stones = buff
+		// Set the next iterantion queue.
+		stoneQueue = next
+
+		// Check the final count of stones. I don't even care what they are,
+		// only how many of each there are.
+		total := 0
+		for _, c := range stoneQueue {
+			total += c
+		}
+		fmt.Printf("Blink %v: %v stones\n", i+1, total)
 	}
 
-	return stones
+	// Check the final count of stones. I don't even care what they are,
+	// only how many of each there are.
+	total := 0
+	for _, c := range stoneQueue {
+		total += c
+	}
+
+	return total
 }

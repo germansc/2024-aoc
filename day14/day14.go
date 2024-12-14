adf7ebdf7bf403a855629ed6021af82f5fa433bf
germansc <germanscillone@gmail.com>
2024-12-14 03:57:25 -0300

    [day14] Added solution for part 2 of day 14.

--- day14/day14.go
+++ day14/day14.go
@@ -51,68 +51,112 @@ func solve(lines []string) {
 		if n, _ := fmt.Sscanf(line, "p=%d,%d v=%d,%d", &r.p.x, &r.p.y, &r.v.x, &r.v.y); n != 4 {
 			panic("Could not parse line!")
 		}
-
 		robots = append(robots, r)
 	}
+
+	startingRobots := make([]robot, len(robots))
+	copy(startingRobots, robots)
 
 	// Simulate the 100 seconds.
 	for range 100 {
 		for k := range robots {
-			robots[k].p.x = (robots[k].p.x + robots[k].v.x)
-			robots[k].p.y = (robots[k].p.y + robots[k].v.y)
-			if robots[k].p.x < 0 {
-				robots[k].p.x += width
-			}
-			if robots[k].p.y < 0 {
-				robots[k].p.y += height
-			}
-			if robots[k].p.x >= width {
-				robots[k].p.x -= width
-			}
-			if robots[k].p.y >= height {
-				robots[k].p.y -= height
-			}
+			moveRobot(&robots[k], width, height)
 		}
 	}
 
-	m := make(map[point][]int)
-	for k, r := range robots {
-		m[r.p] = append(m[r.p], k)
+	m := make(map[point]int)
+	for _, r := range robots {
+		m[r.p]++
 	}
-
-	plotMap(m, width, height)
 
 	q1, q2, q3, q4 := 0, 0, 0, 0
 	for p, count := range m {
 		if p.x < (width-1)/2 {
 			if p.y < (height-1)/2 {
-				q1 += len(count)
+				q1 += count
 			} else if p.y > (height-1)/2 {
-				q3 += len(count)
+				q3 += count
 			}
 		} else if p.x > (width-1)/2 {
 			if p.y < (height-1)/2 {
-				q2 += len(count)
+				q2 += count
 			} else if p.y > (height-1)/2 {
-				q4 += len(count)
+				q4 += count
 			}
 		}
 	}
 
-	// Part 1.
+	// Part 2.
+	// Positions of robots repeat after 10403 iterations.
+	// Search for the time of least overlaps? assuming that most robots will be
+	// forming the tree?
+	least := len(startingRobots)
+	frame := make(map[point]int)
+	leastTime := 0
+	for time := range 10403 {
+		m2 := make(map[point]int)
+		for k := range startingRobots {
+			moveRobot(&startingRobots[k], width, height)
+			m2[startingRobots[k].p]++
+		}
+
+		// Check if all robots are in different positions.
+		if overlapping := len(startingRobots) - len(m2); overlapping < least {
+			least = overlapping
+			leastTime = time + 1
+
+			// Clear the previous record
+			for k := range frame {
+				delete(frame, k)
+			}
+
+			// make a copy of the current frame.
+			for k, v := range m2 {
+				frame[k] = v
+			}
+		}
+	}
+
+	// Ok, this actually worked. And there was even no overlap in that frame,
+	// although not all robots were part of the tree. Coincidence?
+	plotMap(frame, width, height)
+
+	// Print results.
 	fmt.Printf("Part 1: %d\n", q1*q2*q3*q4)
+	fmt.Printf("Part 2: The frame with the least overlap (%v) occurred after %v seconds.\n", least, leastTime)
 }
 
-func plotMap(m map[point][]int, w, h int) {
+func plotMap(m map[point]int, w, h int) {
 	fmt.Print("\033[H\033[2J")
 	for y := range h {
 		for x := range w {
 			if id, ok := m[point{x, y}]; ok {
-				fmt.Print(len(id))
+				fmt.Print(id)
 			} else {
 				fmt.Print(".")
 			}
 		}
 		fmt.Println()
+	}
+}
+
+func moveRobot(r *robot, width, height int) {
+	r.p.x = (r.p.x + r.v.x)
+	r.p.y = (r.p.y + r.v.y)
+
+	if r.p.x < 0 {
+		r.p.x += width
+	}
+
+	if r.p.y < 0 {
+		r.p.y += height
+	}
+
+	if r.p.x >= width {
+		r.p.x -= width
+	}
+
+	if r.p.y >= height {
+		r.p.y -= height
 	}
 }
